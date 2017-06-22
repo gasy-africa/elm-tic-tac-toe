6d97b66fd5112512189b164752c176e42cab7065
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-06-22 19:33:12 -0400

    Add basic tic-tac-toe play functionality

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,15 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, text)
+import Html exposing (Html, div, text, button)
 import Html.Events exposing (onClick)
-import List.Extra exposing (setAt)
+import List.Extra exposing (setAt, getAt)
 import Maybe exposing (withDefault)
+import String exposing (toUpper)
+
+
+-- INTERNAL MODULES
+
+import Styles exposing (..)
 
 
 -- MODEL
@@ -14,35 +20,88 @@ type alias Model =
     , player2 : String
     , turn : String
     , board : List String
+    , isBoardFull : Bool
+    , hasSetPlayer : Bool
     }
+
+
+initModel : Model
+initModel =
+    Model "X" "O" "X" [ "", "", "", "", "", "", "", "", "" ] False False
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "X" "O" "X" [ "", "", "", "", "", "", "", "", "" ], Cmd.none )
+    ( initModel, Cmd.none )
 
 
-type Msg
-    = Mark Int
+
+-- UPDATE HELPERS
+
+
+handleChangeTurn : String -> String
+handleChangeTurn turn =
+    case turn of
+        "X" ->
+            "O"
+
+        _ ->
+            "X"
+
+
+handleBoardFull : List String -> Bool
+handleBoardFull =
+    not << List.any (\v -> v == "")
 
 
 
 -- UPDATE
 
 
-changeTurn : String -> String
-changeTurn turn =
-    if turn == "X" then
-        "O"
-    else
-        "X"
+type Msg
+    = Mark Int
+    | Reset
+    | SetPlayer String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Mark int ->
-            ( { model | board = withDefault [] (setAt int model.turn model.board), turn = changeTurn model.turn }, Cmd.none )
+            let
+                nextBoard =
+                    if getValue int model == "" then
+                        withDefault [] (setAt int model.turn model.board)
+                    else
+                        model.board
+
+                nextTurn =
+                    if getValue int model == "" then
+                        handleChangeTurn model.turn
+                    else
+                        model.turn
+
+                isBoardFull =
+                    handleBoardFull nextBoard
+            in
+                { model
+                    | board = nextBoard
+                    , turn = nextTurn
+                    , isBoardFull = isBoardFull
+                }
+                    ! []
+
+        Reset ->
+            initModel ! []
+
+        SetPlayer str ->
+            { model
+                | player1 = str
+                , player2 = handleChangeTurn str
+                , turn = str
+                , hasSetPlayer = True
+            }
+                ! []
 
 
 subscriptions : Model -> Sub Msg
@@ -51,26 +110,53 @@ subscriptions model =
 
 
 
+-- VIEW HELPERS
+
+
+getValue : Int -> Model -> String
+getValue int model =
+    getAt int model.board |> withDefault ""
+
+
+
 -- VIEW
 
 
 view : Model -> Html Msg
 view model =
-    div []
+    div [ container ]
         [ div []
-            [ div [ onClick (Mark 0) ] [ text "box1" ]
-            , div [ onClick (Mark 1) ] [ text "box2" ]
-            , div [ onClick (Mark 2) ] [ text "box3" ]
+            [ if not model.hasSetPlayer then
+                div [ containerChooseButtons ]
+                    [ text (toUpper "Choose Player")
+                    , div [ chooseButtons ]
+                        [ button [ onClick (SetPlayer "X") ] [ text "Player X" ]
+                        , button [ onClick (SetPlayer "O") ] [ text "Player O" ]
+                        ]
+                    ]
+              else
+                div [ containerChooseButtons ] []
             ]
-        , div []
-            [ div [ onClick (Mark 3) ] [ text "box4" ]
-            , div [ onClick (Mark 4) ] [ text "box5" ]
-            , div [ onClick (Mark 5) ] [ text "box6" ]
+        , div [ innerContainer ]
+            [ div [ box, onClick (Mark 0) ] [ text (getValue 0 model) ]
+            , div [ box, onClick (Mark 1) ] [ text (getValue 1 model) ]
+            , div [ box, onClick (Mark 2) ] [ text (getValue 2 model) ]
             ]
-        , div []
-            [ div [ onClick (Mark 6) ] [ text "box7" ]
-            , div [ onClick (Mark 7) ] [ text "box8" ]
-            , div [ onClick (Mark 8) ] [ text "box9" ]
+        , div [ innerContainer ]
+            [ div [ box, onClick (Mark 3) ] [ text (getValue 3 model) ]
+            , div [ box, onClick (Mark 4) ] [ text (getValue 4 model) ]
+            , div [ box, onClick (Mark 5) ] [ text (getValue 5 model) ]
+            ]
+        , div [ innerContainer ]
+            [ div [ box, onClick (Mark 6) ] [ text (getValue 6 model) ]
+            , div [ box, onClick (Mark 7) ] [ text (getValue 7 model) ]
+            , div [ box, onClick (Mark 8) ] [ text (getValue 8 model) ]
+            ]
+        , div [ buttons ]
+            [ if model.isBoardFull then
+                button [ onClick Reset ] [ text "Reset" ]
+              else
+                text ""
             ]
         ]
 
---
+++ src/Styles.elm
@@ -0,0 +1,59 @@
+module Styles exposing (..)
+
+import Html
+import Html.Attributes exposing (style)
+
+
+container : Html.Attribute msg
+container =
+    style
+        [ ( "display", "flex" )
+        , ( "flex-direction", "column" )
+        , ( "justify-content", "center" )
+        , ( "align-items", "center" )
+        , ( "widht", "100%" )
+        , ( "height", "100vh" )
+        ]
+
+
+innerContainer : Html.Attribute msg
+innerContainer =
+    style
+        [ ( "display", "flex" )
+        ]
+
+
+box : Html.Attribute msg
+box =
+    style
+        [ ( "width", "100px" )
+        , ( "height", "100px" )
+        , ( "border", "1px solid black" )
+        , ( "display", "flex" )
+        , ( "justify-content", "center" )
+        , ( "align-items", "center" )
+        ]
+
+
+buttons : Html.Attribute msg
+buttons =
+    style
+        [ ( "padding-top", "20px" )
+        , ( "min-height", "40px" )
+        ]
+
+
+chooseButtons : Html.Attribute msg
+chooseButtons =
+    style
+        [ ( "display", "flex" )
+        , ( "justify-content", "space-between" )
+        , ( "padding-bottom", "20px" )
+        ]
+
+
+containerChooseButtons : Html.Attribute msg
+containerChooseButtons =
+    style
+        [ ( "min-height", "60px" )
+        ]
